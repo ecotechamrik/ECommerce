66e688c6faf1d296a413f43ae20601f41937c645
Amrik Singh <amriksg81@gmail.com>
2020-07-07 10:32:35 -0700

    Website Model Added...

---
+++ EcoTechAPIs/Models/WebsiteInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcoTechAPIs.Models
+{
+    public class WebsiteInfo
+    {
+        public int WebsiteID { get; set; }
+        public string Name { get; set; }
+        public string URL { get; set; }
+        public string CpanelUser { get; set; }
+        public string CpanelPassword { get; set; }
+        public int BannerWebsiteTitle { get; set; }
+        public int BannerWebsiteTagLine { get; set; }
+        public int CompanyName { get; set; }
+
+    }
+}
